154d6a5272a9f0ac9029a71f6f5d13c002ef1762
Valentin Bas <vbas@persistant.fr>
2014-10-24 14:11:50 +0200

    add error message if ADTROOT not found

--- bleBump/MainWindow.xaml.cs
+++ bleBump/MainWindow.xaml.cs
@@ -19,128 +19,137 @@ using System.Windows.Shapes;
 
 namespace bleBump
 {
-    /// <summary>
-    /// Logique d'interaction pour MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        private Task    m_Task;
-        private Process m_Process;
-        private bool    m_Running;
+	/// <summary>
+	/// Logique d'interaction pour MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private Task m_Task;
+		private Process m_Process;
+		private bool m_Running;
 
-        const int CTRL_C_EVENT = 0;
-        const int CTRL_BREAK_EVENT = 1;
+		const int CTRL_C_EVENT = 0;
+		const int CTRL_BREAK_EVENT = 1;
 
-        const string m_ChangeDirStr = "cd %ADTROOT%\\sdk\\platform-tools & ";
+		string m_ChangeDirStr;
 
-        public MainWindow()
-        {
-            InitializeComponent();
-            m_Running = false;
-            Closing += OnWindowClosing;
-        }
+		public MainWindow()
+		{
+			InitializeComponent();
+			m_Running = false;
+			Closing += OnWindowClosing;
+			string adtroot = Environment.GetEnvironmentVariable("ADTROOT", EnvironmentVariableTarget.Machine);
+			if (adtroot == null || adtroot == "")
+			{
+				logText.Text = "Error : Could not find ADTROOT";
+			}
+			else
+			{
+				m_ChangeDirStr = "cd " + adtroot + "\\sdk\\platform-tools & ";
+			}
+		}
 
-        public void OnWindowClosing(object sender, CancelEventArgs e)
-        {
-            if (m_Running == true)
-            {
-                Process tempProcess;
+		public void OnWindowClosing(object sender, CancelEventArgs e)
+		{
+			if (m_Running == true)
+			{
+				Process tempProcess;
 
-                ExecuteCommand(m_ChangeDirStr + "taskkill /im adb.exe /f /t",
-                               out tempProcess, true);
-                m_Process.WaitForExit();
-                m_Process.Close();
-                m_Task.Wait();
-            }
-        }
+				ExecuteCommand(m_ChangeDirStr + "taskkill /im adb.exe /f /t",
+							   out tempProcess, true);
+				m_Process.WaitForExit();
+				m_Process.Close();
+				m_Task.Wait();
+			}
+		}
 
-        static void ExecuteCommand(string command, out Process process, bool waitAndClose)
-        {
-            ProcessStartInfo processInfo;
+		static void ExecuteCommand(string command, out Process process, bool waitAndClose)
+		{
+			ProcessStartInfo processInfo;
 
-            processInfo = new ProcessStartInfo("cmd.exe", "/c " + command);
-            processInfo.CreateNoWindow = false;
-            processInfo.UseShellExecute = false;
-            // *** Redirect the output ***
-            processInfo.RedirectStandardError = false;
-            processInfo.RedirectStandardOutput = false;
+			processInfo = new ProcessStartInfo("cmd.exe", "/c " + command);
+			processInfo.CreateNoWindow = false;
+			processInfo.UseShellExecute = false;
+			// *** Redirect the output ***
+			processInfo.RedirectStandardError = false;
+			processInfo.RedirectStandardOutput = false;
 
-            process = Process.Start(processInfo);
+			process = Process.Start(processInfo);
 
-            if (waitAndClose)
-            {
-                process.WaitForExit();
-                //int exitCode = process.ExitCode;
-                process.Close();
-            }
-            // *** Read the streams ***
-            //output = process.StandardOutput.ReadToEnd();
-            //error =  process.StandardError.ReadToEnd();
-        }
+			if (waitAndClose)
+			{
+				process.WaitForExit();
+				//int exitCode = process.ExitCode;
+				process.Close();
+			}
+			// *** Read the streams ***
+			//output = process.StandardOutput.ReadToEnd();
+			//error =  process.StandardError.ReadToEnd();
+		}
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            if (!m_Running)
-            {
-                Process tempProcess;
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			if (!m_Running)
+			{
+				Process tempProcess;
 
-                ExecuteCommand(m_ChangeDirStr + "adb shell cd sdcard ; mv btsnoop_hci.log btsnoop_hci_save_" +
-                               DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalSeconds.ToString() +
-                               ".log", out tempProcess, true);
-                ExecuteCommand(m_ChangeDirStr + "adb shell am start -a android.bluetooth.adapter.action.REQUEST_ENABLE",
-                               out tempProcess, true);
-                Action<object> action = (object obj) =>
-                {
-                    Environment.SetEnvironmentVariable("BLEDUMPROOTDIR", Directory.GetCurrentDirectory());
-                    //string filter = "\"BluetoothLeService\"," + "\"BluetoothAdapter\"," + "\"BluetoothGatt\"";
-                    string filter = "";
+				ExecuteCommand(m_ChangeDirStr + "adb shell cd sdcard ; mv btsnoop_hci.log btsnoop_hci_save_" +
+							   DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalSeconds.ToString() +
+							   ".log", out tempProcess, true);
+				ExecuteCommand(m_ChangeDirStr + "adb shell am start -a android.bluetooth.adapter.action.REQUEST_ENABLE",
+							   out tempProcess, true);
+				Action<object> action = (object obj) =>
+				{
+					Environment.SetEnvironmentVariable("BLEDUMPROOTDIR", Directory.GetCurrentDirectory());
+					//string filter = "\"BluetoothLeService\"," + "\"BluetoothAdapter\"," + "\"BluetoothGatt\"";
+					string filter = "";
 
-                    if (filter.Length != 0)
-                        filter = " -s " + filter;
-                    ExecuteCommand(m_ChangeDirStr + "adb logcat -v time" + filter + "> %BLEDUMPROOTDIR%\\test.txt",
-                                   out m_Process, false);
-                };
+					if (filter.Length != 0)
+						filter = " -s " + filter;
+					ExecuteCommand(m_ChangeDirStr + "adb logcat -v time" + filter + "> %BLEDUMPROOTDIR%\\test.txt",
+								   out m_Process, false);
+				};
 
-                m_Task = Task.Factory.StartNew(action, "adbLogcat");
-                startButton.Content = "Stop";
-                stateLabel.Text = "Running";
-                m_Running = true;
-            }
-            else
-            {
-                Process tempProcess;
+				m_Task = Task.Factory.StartNew(action, "adbLogcat");
+				startButton.Content = "Stop";
+				stateLabel.Text = "Running";
+				m_Running = true;
+			}
+			else
+			{
+				Process tempProcess;
 
-                ExecuteCommand(m_ChangeDirStr + "taskkill /im adb.exe /f /t",
-                               out tempProcess, true);
-                m_Process.WaitForExit();
-                int exitCode = m_Process.ExitCode;
-                m_Process.Close();
-                m_Task.Wait();
-                logText.Text = Directory.GetCurrentDirectory();
+				ExecuteCommand(m_ChangeDirStr + "taskkill /im adb.exe /f /t",
+							   out tempProcess, true);
+				m_Process.WaitForExit();
+				int exitCode = m_Process.ExitCode;
+				m_Process.Close();
+				m_Task.Wait();
+				logText.Text = Directory.GetCurrentDirectory();
 
-                ExecuteCommand(m_ChangeDirStr + "adb pull /sdcard/btsnoop_hci.log %BLEDUMPROOTDIR%",
-                               out tempProcess, true);
-                logText.Text += "\n" + ("ExitCode: " + exitCode.ToString()) + "\n\n";
-                string logFile = "";
-                try
-                {
-                    using (StreamReader sr = new StreamReader(Directory.GetCurrentDirectory() + "\\test.txt"))
-                    {
-                        while (sr.Peek() >= 0)
-                        {
-                            logFile += sr.ReadLine() + "\n";
-                        }
-                    }
-                }
-                catch (Exception exception) 
-                {
-                    logText.Text += "Can't open the file : " + exception.ToString();
-                }
-                logText.Text += logFile;
-                startButton.Content = "Start";
-                stateLabel.Text = "Stopped";
-                m_Running = false;
-            }
-        }
-    }
+				ExecuteCommand(m_ChangeDirStr + "adb pull /sdcard/btsnoop_hci.log %BLEDUMPROOTDIR%",
+							   out tempProcess, true);
+				logText.Text += "\n" + ("ExitCode: " + exitCode.ToString()) + "\n\n";
+				string logFile = "";
+				try
+				{
+					using (StreamReader sr = new StreamReader(Directory.GetCurrentDirectory() + "\\test.txt"))
+					{
+						while (sr.Peek() >= 0)
+						{
+							logFile += sr.ReadLine() + "\n";
+						}
+					}
+				}
+				catch (Exception exception)
+				{
+					logText.Text += "Can't open the file : " + exception.ToString();
+				}
+				logText.Text += logFile;
+				startButton.Content = "Start";
+				stateLabel.Text = "Stopped";
+				m_Running = false;
+			}
+		}
+	}
 }
